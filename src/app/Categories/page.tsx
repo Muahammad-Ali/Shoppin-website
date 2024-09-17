2d0c2fcc1ffae284192f875a648f0e9b2d07a26f
Muhammad Ali <muhammadaliuet9@gmail.com>
2024-09-17 12:36:24 +0500

    code push

--- src/app/Categories/page.tsx
+++ src/app/Categories/page.tsx
@@ -7,6 +7,9 @@ const Categories: React.FC = () => {
           <button className="px-2 py-2 text-white bg-[#ff8f9c] rounded shadow-md hover:bg-[#ff6f72] hover:shadow-lg transition duration-300" type="button">
             Computers and laptops
           </button>
+          <button className="px-2 py-2 text-white bg-[#ff8f9c] rounded shadow-md hover:bg-[#ff6f72] hover:shadow-lg transition duration-300" type="button">
+            Computers and laptops
+          </button>
       
           <button className="px-4 py-4 text-white bg-[#ff8f9c] rounded shadow-md hover:bg-[#ff6f72] hover:shadow-lg transition duration-300" type="button">
             Mobiles & Tablets
